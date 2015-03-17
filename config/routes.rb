4df6a83f7cdde9423a471ac3f0d7686ba98d4b10
Ahmed <ahmedfay@hotmail.com>
2015-03-17 17:01:01 -0400

    Update votes and devise routes from PUT and DELETE to GET

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :links
+
+  devise_for :users, path: '', path_names: {sign_in: 'signin', sign_out: 'signout'}
+
+
   root "links#index"
+
+  resources :links do
+    member do
+      get "like", to:    "links#upvote"
+      get "dislike", to: "links#downvote"
+    end
+  end
 end
