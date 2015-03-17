0eae6f564dd6f326b7087ba961a80db7dbeb69e5
Ahmed <ahmedfay@hotmail.com>
2015-03-17 17:40:53 -0400

    Add comments for links

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
 
+  resources :comments
+
   devise_for :users, path: '', path_names: {sign_in: 'signin', sign_out: 'signout'}
 
 
   root "links#index"
 
   resources :links do
+    resources :comments
     member do
       get "like", to:    "links#upvote"
       get "dislike", to: "links#downvote"
